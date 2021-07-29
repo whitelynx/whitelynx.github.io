4fba6f9626376f016a89daf81eb2c21b44f9d57b
David H. Bronke <whitelynx@gmail.com>
2021-07-28 21:33:12 -0400

    fix(.vuepress/config.js): use default theme config if last grub answers missing

--- .vuepress/config.js
+++ .vuepress/config.js
@@ -1,10 +1,34 @@
 const fs = require('fs');
 
 
+const defaultThemeConfig = {
+  email: '',
+  address1: '',
+  address2: '',
+  city: '',
+  state: '',
+  zip: '',
+  country: '',
+  url: 'https://whitelynx.github.io',
+  phoneNumber: '',
+  confirm: true,
+  stateAbbr: '',
+  addressQuery: '',
+  showContactInfo: false
+};
+
+let themeConfig = defaultThemeConfig;
+try {
+    themeConfig = JSON.parse(fs.readFileSync('.last-gulp-answers.json'));
+} catch(exc) {
+    console.warn('Exception reading .last-gulp-answers.json; using default theme config.', exc);
+}
+
+
 module.exports = {
     title: 'David H. Bronke',
     description: 'Software architect/engineer',
-    themeConfig: JSON.parse(fs.readFileSync('.last-gulp-answers.json')),
+    themeConfig,
     extendMarkdown(md) {
         md.use(require('markdown-it-deflist'));
     }
