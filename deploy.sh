40439fa9e639b3babceac5991e2ecccef4d68172
David H. Bronke <whitelynx@gmail.com>
2021-07-28 20:56:13 -0400

    Don't wipe old deploy history every time we deploy!

--- deploy.sh
+++ deploy.sh
@@ -3,6 +3,11 @@
 # abort on errors
 set -e
 
+cd .vuepress
+rm -r dist
+git clone -b master $(git remote get-url origin) dist
+cd ..
+
 # build
 npm run build
 
@@ -11,8 +16,13 @@ cd .vuepress/dist
 # if you are deploying to a custom domain
 # echo 'www.example.com' > CNAME
 
-git init
 git add -A
 git commit -m "deploy $(date -I)"
+git push
 
-git push -f git@github.com:whitelynx/whitelynx.github.io.git master
+# OLD: Only use the first time, or it will overwrite all previous deploy history!
+#git init
+#git add -A
+#git commit -m "deploy $(date -I)"
+#
+#git push -f git@github.com:whitelynx/whitelynx.github.io.git master
